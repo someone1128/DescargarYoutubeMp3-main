eaf7064b968bad46823ac78267d4f1c96ad840a6
hzy <3500412648@qq.com>
2023-09-07 14:26:12 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useRef, useState } from "react"
+import { youtube_parser } from "./utils";
+
+function App() {
+  const inputUrlRef = useRef();
+  const [urlResult, setUrlResult] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("inputUrlRef.current.value",inputUrlRef.current.value);
+    const youtubeID = youtube_parser(inputUrlRef.current.value);
+
+    const options = {
+      method: 'get',
+      url: 'https://youtube-mp36.p.rapidapi.com/dl',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+      },
+      params: {
+        id: youtubeID
+      }
+    }
+    axios(options)
+      .then(res => setUrlResult(res.data.link))
+      .catch(err => console.log(err))
+
+    inputUrlRef.current.value = '';
+
+  }
+
+  return (
+    <div className="app">
+      {/*<span className="logo">Nahuel Astudillo</span>*/}
+      <section className="content">
+        <h1 className="content_title">Descargar MP3 gratis de youtube</h1>
+        <p className="content_description">
+          Copias el link de tu cancion en youtube y lo pegas acá.
+        </p>
+
+        <form onSubmit={handleSubmit} className="form">
+          <input ref={inputUrlRef} placeholder="Pegar link del video de youtube..." className="form_input" type="text" />
+          <button type="submit" className="form_button">Procesar</button>
+        </form>
+
+        {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult} className="download_btn">Download MP3</a> : ''}
+
+      </section>
+      <div className="footer">
+        <a href="https://code.aiconver.cn/" target="_blank" rel="noopener noreferrer">Code Converter</a>
+        <a href="https://base64.aiconver.cn/" target="_blank" rel="noopener noreferrer">Base64 Converter</a>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
